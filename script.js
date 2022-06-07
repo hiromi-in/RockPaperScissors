edb14622749be778b4590ebb128cbd14b5ec45b5
hiromi-in <piromi.ishikawa@gmail.com>
2022-06-07 14:28:16 -0500

    Create variables for guest input and computer selection.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+('use strict');
 
+let guestJanken = window.prompt('Please pick and type one out of rock, paper or scissors.');
 
 let janken = ['rock','paper','scissors'];
 let randJanken = janken[Math.floor(Math.random()*janken.length)];
 
+console.log(guestJanken);
+console.log(randJanken);
