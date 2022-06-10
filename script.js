83b252d7d2a5fdfaa4328ec318bbfb82e9595072
hiromi-in <piromi.ishikawa@gmail.com>
2022-06-10 16:29:30 -0500

    Finalize the game() function

--- script.js
+++ script.js
@@ -6,38 +6,41 @@ let i = 0;
 
 
 function game(){
-       
-
 
        window.addEventListener('click',function(e){
             const playerSelection = (e.target).dataset.janken;
-       
-        console.log(playerSelection);
-
-        const janken = ['rock','paper','scissors'];
-        const computerSelection = janken[Math.floor(Math.random()*janken.length)];
-       
-        console.log(computerSelection);
-
-        function playRound(playerSelection, computerSelection) {
     
-            if(playerSelection===computerSelection){
-                return `Tie! ${playerSelection} and ${computerSelection}. Try again :)`;
-            } else if(playerSelection==='rock'&&computerSelection==='scissors'||playerSelection==='scissors'&&computerSelection==='paper'||playerSelection==='paper'&&computerSelection==='rock'){
-                playerScore += 1;
-                return `You won! ${playerSelection} beats ${computerSelection}. Congratulations!:D`;
-            } else{
-                computerScore += 1;
-                return`You lost! ${playerSelection} is beaten by ${computerSelection}. Try again. :)`;
+            const janken = ['rock','paper','scissors'];
+            const computerSelection = janken[Math.floor(Math.random()*janken.length)];
+    
+            function playRound(playerSelection, computerSelection) {
+        
+                if(playerSelection===computerSelection){
+                    return `Tie! ${playerSelection} and ${computerSelection}. Try again :)`;
+                } else if(playerSelection==='rock'&&computerSelection==='scissors'||playerSelection==='scissors'&&computerSelection==='paper'||playerSelection==='paper'&&computerSelection==='rock'){
+                    playerScore += 1;
+                    return `You won! ${playerSelection} beats ${computerSelection}. Congratulations!:D`;
+                } else{
+                    computerScore += 1;
+                    return`You lost! ${playerSelection} is beaten by ${computerSelection}. Try again. :)`;
+                }
             }
-        }
+            i += 1;
+            this.document.getElementById('message').textContent= `This is the ${i}th challenge. ${playRound(playerSelection,computerSelection)}. Your score is ${playerScore} and computer score is ${computerScore}.`;
         
-        i += 1;
-      
-        this.document.getElementById('message').textContent= `This is the ${i}th challenge. ${playRound(playerSelection,computerSelection)}. Your score is ${playerScore} and computer score is ${computerScore}.`;
-        
-    })
+        })
+   
+
 };
 
-game()
+//do{
+//game();
+//}
+//while(playerScore<5 && computerScore<5);
+//
+//if (playerScore===5){
+//    document.getElementById('h2').textContent= 'You did it!! You beat the machine!';
+//} else if (computerScore===5){
+//    document.getElementById('h2').textContent= 'Computer got you this time. Try it again!';
+//};
 
