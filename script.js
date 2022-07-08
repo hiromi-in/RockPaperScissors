bbadf6859474daea9e7f65212fdf5db9287c4e32
hiromi-in <piromi.ishikawa@gmail.com>
2022-07-08 15:48:43 -0500

    Solve 'undefined' comment issue.

--- script.js
+++ script.js
@@ -48,14 +48,18 @@ function game(){
                     playerScore += 1;
                     document.getElementById('y_point').textContent = playerScore;
                     return `You won! ${playerSelection} beats ${computerSelection}. Congratulations!:D`;
+                    
                 }  else if (playerSelection==='rock'&&computerSelection==='paper'||playerSelection==='scissors'&&computerSelection==='rock'||playerSelection==='paper'&&computerSelection==='scissors'){
                     computerScore += 1;
                     document.getElementById('c_point').textContent = computerScore;
                     return`You lost! ${playerSelection} is beaten by ${computerSelection}. Try again. :)`;
-                } 
+
+                } else {
+                    return `Please pick one of the cats!`
+                }
             }
             //i += 1;
-            this.document.getElementById('message').textContent= `${playRound(playerSelection,computerSelection)}.`;
+            this.document.getElementById('message').textContent= `${playRound(playerSelection,computerSelection)}`;
             
             if (playerScore===5 || computerScore ===5){
             gameOver();
