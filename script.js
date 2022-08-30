848455ac9d46d53afcf42771a7d7734f97c9c703
hiromi-in <piromi.ishikawa@gmail.com>
2022-08-30 11:24:39 -0500

    Create animation for the loading screen.

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 ('use strict');
 
+let bar = new ProgressBar.Line(splash_text,{
+    easing: 'easeInOut',
+    duration: 1000,
+    strokeWidth: 0.2,
+    color: #555,
+    trailWidth: 0.2;
+    trailColor: '#bbb',
+    text:{
+        style:{
+            position: 'absolute',
+            left: '50%',
+            top: '50%',
+            padding: '0',
+            margin: '-30px 0 0 0',
+            transform: 'translate(-50%, -50%)',
+            'font-size': '1rem',
+            color: '#fff',
+        },
+    autoStyleContainer: false
+    },
+    step: function(state, bar){
+        bar.setText(Math.round(bar.value()* 100) + ' %');
+    }
+});
+
+bar.animate(1.0, function(){
+    $("splash_text").fadeOut(10);
+    $(".loader_cover-up").addClass("coveranime");
+    $(".loader_cover-down").addClass("coveranime");
+    $("#splash").fadeOut();
+})
+
+
+
 let playerScore = 0;
 let computerScore = 0;
 //let i = 0;
