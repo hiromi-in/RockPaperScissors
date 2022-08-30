27d86f949de24906d19cad5212fda5d38a820a5d
hiromi-in <piromi.ishikawa@gmail.com>
2022-08-30 12:05:08 -0500

    Add loading screen.

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
 ('use strict');
 
-let bar = new ProgressBar.Line(splash_text,{
-    easing: 'easeInOut',
-    duration: 1000,
-    strokeWidth: 0.2,
-    color: #555,
-    trailWidth: 0.2;
-    trailColor: '#bbb',
-    text:{
-        style:{
-            position: 'absolute',
-            left: '50%',
-            top: '50%',
-            padding: '0',
-            margin: '-30px 0 0 0',
-            transform: 'translate(-50%, -50%)',
-            'font-size': '1rem',
-            color: '#fff',
-        },
-    autoStyleContainer: false
-    },
-    step: function(state, bar){
-        bar.setText(Math.round(bar.value()* 100) + ' %');
-    }
+var bar = new ProgressBar.Line(splash_text, {//id名を指定
+	easing: 'easeInOut',//アニメーション効果linear、easeIn、easeOut、easeInOutが指定可能
+	duration: 2000,//時間指定(1000＝1秒)
+	strokeWidth: 0.2,//進捗ゲージの太さ
+	color: '#fff',//進捗ゲージのカラー
+	trailWidth: 0.2,//ゲージベースの線の太さ
+	trailColor: '#bbb',//ゲージベースの線のカラー
+	text: {//テキストの形状を直接指定				
+		style: {//天地中央に配置
+			position: 'absolute',
+			left: '50%',
+			top: '50%',
+			padding: '0',
+			margin: '-30px 0 0 0',//バーより上に配置
+			transform:'translate(-50%,-50%)',
+			'font-size':'1rem',
+			color: '#fff',
+		},
+		autoStyleContainer: false //自動付与のスタイルを切る
+	},
+	step: function(state, bar) {
+		bar.setText(Math.round(bar.value() * 100) + ' %'); //テキストの数値
+	}
 });
+
+
 
 bar.animate(1.0, function(){
     $("splash_text").fadeOut(10);
     $(".loader_cover-up").addClass("coveranime");
     $(".loader_cover-down").addClass("coveranime");
     $("#splash").fadeOut();
+    $("#container").fadeIn();
 })
 
 
